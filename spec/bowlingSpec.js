21181645061a521e307bbce3055b7150fc2eda26
ria patel <ria.amy.patel@gmail.com>
2020-02-01 21:59:30 +0000

    cumulative score functionality added

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -12,4 +12,18 @@ describe('Bowling', function() {
       expect(bowling.latestScore()).toEqual(0);
     });
   });
+
+  describe('returns a valid score', function() {
+    it('of a frame (two balls)', function() {
+      expect(bowling.bowl(1, 2)).toEqual(3);
+    });
+
+    it('returns a cumulative score', function() {
+      var frame1 = bowling.bowl(1, 2);
+      var frame2 = bowling.bowl(1, 2);
+      bowling.addScore(frame1);
+      bowling.addScore(frame2);
+      expect(bowling.latestScore()).toEqual(6)
+    }); 
+  });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -7,3 +7,11 @@ function Bowling() {
 Bowling.prototype.latestScore = function() {
   return this.score;
 };
+
+Bowling.prototype.bowl = function(ball1, ball2) {
+  return ball1 + ball2;
+}
+
+Bowling.prototype.addScore = function(frame) {
+  return this.score += frame;
+}
